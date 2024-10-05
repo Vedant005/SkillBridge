53f0396d410384d5272f3db0d515506c21bb3e44
Vedant005 <vedantkanekar909@gmail.com>
2024-10-06 01:04:28 +0530

    Freelancer filters for client side added

---
+++ frontend/src/components/FreelanceFilter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function FreelanceFilter() {
+  return (
+    <div>
+      <div>hourly rate</div>
+      <div>Location</div>
+      <div>Time zone</div>
+      <div>Categories</div>
+      <div>Experrinece level</div>
+      <div>Ratings</div>
+    </div>
+  );
+}
+
+export default FreelanceFilter;
