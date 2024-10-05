4f0670247a6c6a3bd04175fc908ffaef77a5532e
Vedant005 <vedantkanekar909@gmail.com>
2024-10-06 01:03:30 +0530

    gigs filter skeleton

---
+++ frontend/src/components/GigsFilter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function GigsFilter() {
+  return (
+    <div>
+      <div>hourly rate</div>
+      <div>Location</div>
+      <div>Time zone</div>
+      <div>Categories</div>
+      <div>Experrinece level</div>
+    </div>
+  );
+}
+
+export default GigsFilter;
