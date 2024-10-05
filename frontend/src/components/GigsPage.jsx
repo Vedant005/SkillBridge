ddcbfe39ae5c409f0bc81d9d84cdc56340d58484
Vedant005 <vedantkanekar909@gmail.com>
2024-10-06 01:03:19 +0530

    Freelacer pages

---
+++ frontend/src/components/GigsPage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function GigsPage() {
+  return (
+    <div>
+      <h3>When posted</h3>
+      <div>
+        <h1>Job title</h1>
+      </div>
+
+      <div>
+        <div>Ratings</div>
+        <div>Location</div>
+      </div>
+      <div>
+        <div className="flex">
+          <h1>Price details</h1>
+        </div>
+        <div>
+          <h3>Experience level</h3>
+        </div>
+        <div>
+          <h3>Estimated Budget</h3>
+        </div>
+      </div>
+
+      <div>
+        <h2>Job description</h2>
+      </div>
+
+      <div>Skills tags</div>
+    </div>
+  );
+}
+
+export default GigsPage;
---
+++ frontend/src/pages/Freelancer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ClientFilter from "../components/ClientFilter";
+
+function Freelancer() {
+  return <div>Freelancer</div>;
+}
+
+export default Freelancer;
