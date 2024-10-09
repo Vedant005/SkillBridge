a9f6212b7f48a15aa38b19c840bf74ad98914a12
Vedant005 <vedantkanekar909@gmail.com>
2024-10-09 18:56:41 +0530

    formatting

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -1,93 +1,86 @@
+const { string } = require("joi");
+const mongoose = require("mongoose");
 
-const { string } = require("joi")
-const mongoose=require("mongoose")
-
-const userSchema=mongoose.Schema({
-    username:{
-        type: String,
-        trim:true,
-        required:true
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
     },
-    email:{
-        type: String,
-        trim:true,
-        required:true
+    email: {
+      type: String,
+      trim: true,
+      required: true,
     },
-    password:{
-        type: String,
-        trim:true,
-        required:true
+    password: {
+      type: String,
+      trim: true,
+      required: true,
     },
-    image:{
-        type:String,
-       
-     },
-     is_online:{
-        type:String,
-        default:'0'
-     },
-
-    otp:{
-
-        type:String,
-      
+    image: {
+      type: String,
+    },
+    is_online: {
+      type: String,
+      default: "0",
     },
 
-
-    otpexpire:{
-        type:Date
-    },
-    otpVerifiy:{
-        type:Boolean,
-        default:false
-    },
-    domain:{
-        type :String,
-        trim:true,
-       
-    },
-    skills:{
-        type:[String],
-        default:[]
-       
-    },
-   
-    education:{
-        type:String,
-     
-    },
-    location:{
-       
-        type:String,
-       
-    },
-    description:{
-        type:String,
-        
-    },
-    reviews:{
-        type:Object,
-    },
-    isclient:{
-        type:Boolean,
-        default:false
+    otp: {
+      type: String,
     },
 
-    order:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"jobmodel"
+    otpexpire: {
+      type: Date,
     },
-    portfolio:{
-        type:String
-    }
+    otpVerifiy: {
+      type: Boolean,
+      default: false,
+    },
+    domain: {
+      type: String,
+      trim: true,
+    },
+    skills: {
+      type: [String],
+      default: [],
+    },
 
-    
- 
+    education: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    reviews: {
+      type: Object,
+    },
+    isclient: {
+      type: Boolean,
+      default: false,
+    },
 
-  
+    order: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "jobmodel",
+    },
+    portfolio: {
+      type: String,
+    },
+    level: {
+      type: String,
+    },
+    ratings: {
+      type: String,
+    },
+    timeZone: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-},
-{timestamps:true}
-)
-
-module.exports=mongoose.model("usermodel",userSchema)
+module.exports = mongoose.model("usermodel", userSchema);
